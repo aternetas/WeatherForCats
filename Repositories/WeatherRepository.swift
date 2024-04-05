007b303a695f58b7a3fdfab76bcb4947a8dd66fe
kot lena <aternetas@gmail.com>
2024-04-05 11:04:44 +0300

    we will get a data from the server

--- Repositories/WeatherRepository.swift
+++ Repositories/WeatherRepository.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+class WeatherRepository {
+    let KEY: String = ""
+    
+    private func getUrl(city: String) -> URL? {
+        URL(string: "https://api.weatherapi.com/v1/current.json?key=\(KEY)&q=\(city)&aqi=no")
+    }
+    
+    private func getWeatherData(city: String) {
+        guard let url = getUrl(city: city) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, let response = response, error == nil else {
+                print(error!.localizedDescription)
+                return
+            }
+            
+            var result: WeatherDataDto?
+            do {
+                result = try JSONDecoder().decode(WeatherDataDto.self, from: data)
+            }
+            catch {
+                print("failed to convert json, error: \(error.localizedDescription)")
+            }
+            
+            guard let result = result else { return }
+        }
+        
+        task.resume()
+    }
+}
