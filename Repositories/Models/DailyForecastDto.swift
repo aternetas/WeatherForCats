2836a52333c60dc229838a3ba2da5bdaf0470094
kot lena <aternetas@gmail.com>
2024-04-08 17:33:54 +0300

    add marks

--- Repositories/Models/DailyForecastDto.swift
+++ Repositories/Models/DailyForecastDto.swift
@@ -8,16 +8,20 @@
 import Foundation
 
 struct DailyForecastDto: Codable {
+    //MARK: -currently used
     let averageDailyWeather: AverageDailyWeatherDto
     
+    //MARK: -currently unused
     let date: String
     let dateEpoch: Int
     let astro: AstroConditionsDto
     let hourlyWeather: HourlyWeatherDto
     
     enum CodingKeys: String, CodingKey {
+        //MARK: -currently used
         case averageDailyWeather = "day"
         
+        //MARK: -currently unused
         case date
         case dateEpoch = "date_epoch"
         case astro
