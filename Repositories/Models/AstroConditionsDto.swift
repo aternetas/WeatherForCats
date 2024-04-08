3149e70901ae389bbfc752c616fd31282b51c0c3
kot lena <aternetas@gmail.com>
2024-04-08 17:56:05 +0300

    expand AstroConditionsDto

--- Repositories/Models/AstroConditionsDto.swift
+++ Repositories/Models/AstroConditionsDto.swift
@@ -7,6 +7,17 @@
 
 import Foundation
 
+//MARK: -currently unused
 struct AstroConditionsDto: Codable {
-    
+    let sunrise, sunset, moonrise, moonset: String
+    let moonPhase: String
+    let moonIllumination, isMoonUp, isSunUp: Int
+
+    enum CodingKeys: String, CodingKey {
+        case sunrise, sunset, moonrise, moonset
+        case moonPhase = "moon_phase"
+        case moonIllumination = "moon_illumination"
+        case isMoonUp = "is_moon_up"
+        case isSunUp = "is_sun_up"
+    }
 }
