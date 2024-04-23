f6764e40e3d466d2337064f74612d2a55f43385a
kot lena <aternetas@gmail.com>
2024-04-23 12:08:44 +0300

    searchTextField's customization

--- Search/SearchViewController.swift
+++ Search/SearchViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    @IBOutlet weak var searchTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,5 +17,17 @@ class SearchViewController: UIViewController {
     }
     
     private func setupUi() {
+        let placeholderAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 17),
+            .foregroundColor: UIColor(resource: .textColor2)
+        ]
+        searchTextField.attributedPlaceholder = NSAttributedString(string: "Какой город?", attributes: placeholderAttributes)
+        
+        let searchIconView = UIImageView(frame: CGRect(x: 7.5, y: 4.5, width: 20, height: 20))
+        searchIconView.image = UIImage(systemName: "magnifyingglass")
+        let searchIconContainerView = UIView(frame: CGRect(x: 0, y: 0, width: 27, height: 27))
+        searchIconContainerView.addSubview(searchIconView)
+        searchTextField.leftView = searchIconContainerView
+        searchTextField.leftViewMode = .always
     }
 }
