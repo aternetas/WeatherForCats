10c775d0aacfd1b926e6aa80e37c1b15b6554751
kot lena <aternetas@gmail.com>
2024-05-03 10:07:08 +0300

    configured trailing action – favourite city deletion

--- Search/FavouriteCityTableViewCell.swift
+++ Search/FavouriteCityTableViewCell.swift
@@ -13,7 +13,6 @@ class FavouriteCityTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
     
     func bind(city: String) {
--- Search/SearchViewController.swift
+++ Search/SearchViewController.swift
@@ -12,19 +12,44 @@ protocol SearchViewControllerProtocol {
 }
 
 class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate {
-
+    
     @IBOutlet private weak var citySearchBar: UISearchBar!
     @IBOutlet weak var favouriteCitiesTableView: UITableView!
     
     var delegate: SearchViewControllerProtocol?
     var cityService: CityServiceProtocol?
     
+    private var favouriteCities: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        getFavouritesCities()
         citySearchBar.delegate = self
         favouriteCitiesTableView.delegate = self
         favouriteCitiesTableView.dataSource = self
+    }
+    
+    private func getFavouritesCities() {
+        favouriteCities = cityService?.getCities() ?? []
+    }
+        
+    //MARK: -UITableViewDelegate
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "🗑") { _, _, completionHandler in
+            self.cityService?.removeCity(city: self.favouriteCities[indexPath.item])
+            self.getFavouritesCities()
+            completionHandler(true)
+            tableView.reloadData()
+        }
+    
+        deleteAction.backgroundColor = UIColor.black
+        
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction])
+        swipeConfiguration.performsFirstActionWithFullSwipe = false
+        
+        return swipeConfiguration
     }
     
     //MARK: -UISearchBarDelegate
@@ -39,16 +64,14 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
     
     //MARK: -UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        cityService?.getCities().count ?? 0
+        favouriteCities.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = favouriteCitiesTableView.dequeueReusableCell(withIdentifier: "FavouriteCityTableViewCell", for: indexPath) as! FavouriteCityTableViewCell
         
-        if let favouriteCities = cityService?.getCities() {
-            cell.bind(city: favouriteCities[indexPath.item])
-            cell.setupUI()
-        }
+        cell.bind(city: favouriteCities[indexPath.item])
+        cell.setupUI()
         
         return cell
     }
